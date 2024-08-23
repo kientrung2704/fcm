91b2ee2774147b6b8c2297d48d0c7d4fa43658c5
Nguyen Trung Kien <nguyentienthanh@codelovers.vn>
2024-08-23 14:39:33 +0700

    push

---
+++ config.ts
@@ -0,0 +1,11 @@
+export const firebaseConfig = {
+  apiKey: "AIzaSyDfh3dniA7xivKyMsVw5C62I56D45Y7sv0",
+  authDomain: "mydio-b0d77.firebaseapp.com",
+  projectId: "mydio-b0d77",
+  storageBucket: "mydio-b0d77.appspot.com",
+  messagingSenderId: "766181932529",
+  appId: "1:766181932529:web:f93ec832ec189b3774d850",
+  measurementId: "G-CVW5SPK67C",
+};
+
+export const vapidKey = "BLFwEkh5rp4G0EYQ2ol0fmPr8UUGc0U4exGuR_5fKrI4EQqg_bi1paMc3gCgy8UCvjGutgrfHNeuvEEO4uAudE0";
